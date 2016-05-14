c10e889d960ed21a4a13b8e3545784f06060206d
Vlad Sydorenko <vladyslav.sydorenko@gmail.com>
2016-05-14 15:24:20 +0300

    Added templates directory

--- cactus/settings.py
+++ cactus/settings.py
@@ -62,7 +62,9 @@ ROOT_URLCONF = 'cactus.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            '/templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
